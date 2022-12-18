ab44543f19b8115aa73667017b20d20b2b219b04
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-12-18 16:09:19 -0600

    Added joke text and get joke button for the home view

--- ChuckNorisJokes/HomeViewController.swift
+++ ChuckNorisJokes/HomeViewController.swift
@@ -13,6 +13,8 @@ protocol HomeViewControllerDelegate: AnyObject {
 
 class HomeViewController: UIViewController {
 
+    let jokeLabel = UILabel()
+    let getJokeButton = UIButton()
     weak var delegate: HomeViewControllerDelegate?
     
     override func viewDidLoad() {
@@ -20,6 +22,37 @@ class HomeViewController: UIViewController {
         title = "Home"
         view.backgroundColor = .secondarySystemBackground
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .done, target: self, action: #selector(didTapMenuButton))
+        setupJokeLabel()
+        setupGetJokeButton()
+    }
+    
+    private func setupJokeLabel() {
+        jokeLabel.text = ""
+        view.addSubview(jokeLabel)
+        jokeLabel.translatesAutoresizingMaskIntoConstraints = false
+        jokeLabel.numberOfLines = 0
+        jokeLabel.lineBreakMode = .byWordWrapping
+        jokeLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        jokeLabel.textColor = .systemRed
+        let centerXConstraint = jokeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let topConstraint = jokeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
+        let widthConstraint = jokeLabel.widthAnchor.constraint(equalToConstant: 300)
+
+        NSLayoutConstraint.activate([centerXConstraint, widthConstraint, topConstraint])
+    }
+    
+    private func setupGetJokeButton() {
+        getJokeButton.setTitle("   Random Chuck Norris Joke   ", for: .normal)
+        getJokeButton.backgroundColor = .systemIndigo
+        view.addSubview(getJokeButton)
+        getJokeButton.frame = CGRect(x: 0, y: 0, width: 200, height: 35)
+        getJokeButton.layer.cornerRadius = getJokeButton.frame.size.height / 2
+        getJokeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let centerXConstraint = getJokeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let bottomConstraint = getJokeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        
+        NSLayoutConstraint.activate([centerXConstraint, bottomConstraint])
     }
 
     @objc func didTapMenuButton() {
